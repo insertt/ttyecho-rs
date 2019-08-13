98b52ca806b5b382a42cbb2af932380b0be8c985
insertt <inserttdev@gmail.com>
2019-08-13 06:52:25 +0200

    Export empty function if target os is not Linux

--- src/lib.rs
+++ src/lib.rs
@@ -16,31 +16,35 @@
 //! }
 //! ```
 //! 
-#![cfg(linux)]
+#[cfg(target_os = "linux")]
 use libc::{ ioctl, open, close, TIOCSTI, O_RDWR };
 
 /// Appends given data into given pseudoterminal buffer by using [ioctl] syscall with [TIOCSTI] parameter.
 /// It will append a null terminator to the tty path if there isn't one, 
-/// because most libc functions expect strings to be null terminated. 
+/// because most libc functions expect strings to be null terminated.
+#[allow(unused_variables)]
 pub fn ttyecho<S: Into<String>>(tty: S, data: S, new_line: bool) {
-    let mut tty = tty.into();
-    let mut command = data.into();
+    #[cfg(target_os = "linux")]
+    {
+        let mut tty = tty.into();
+        let mut command = data.into();
 
-    if ! tty.ends_with('\0') {
-        tty.push('\0');
-    }
-
-    if new_line && ! command.ends_with('\0') {
-        command.push('\r');
-    }
-
-    unsafe {
-        let fd = open(tty.as_ptr() as *const i8, O_RDWR);
-
-        for ch in command.as_bytes() {
-            ioctl(fd, TIOCSTI, ch);
+        if ! tty.ends_with('\0') {
+            tty.push('\0');
         }
-        
-        close(fd);
+
+        if new_line && ! command.ends_with('\0') {
+            command.push('\r');
+        }
+
+        unsafe {
+            let fd = open(tty.as_ptr() as *const i8, O_RDWR);
+
+            for ch in command.as_bytes() {
+                ioctl(fd, TIOCSTI, ch);
+            }
+            
+            close(fd);
+        }
     }
 }
